2e01eaf6cfa988ed8436d7321be5dab3bfd398a2
Fluva74 <85516975+Fluva74@users.noreply.github.com>
2021-12-23 11:27:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -56,3 +56,27 @@ app.use("/users", require("./routes/users.js"));
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server running on  ${PORT}`));
+
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+});
+
+// Routes
+app.use("/", require("./routes/index.js"));
+app.use("/users", require("./routes/users.js"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
