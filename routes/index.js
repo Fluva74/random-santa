34e7a72d00298437f15e8a860fca134be1356e6e
Fluva74 <85516975+Fluva74@users.noreply.github.com>
2021-12-24 10:57:16 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,23 @@ const { ensureAuthenticated, forwardAuthenticated } = require("../config/auth");
 // Welcome Page
 router.get("/", forwardAuthenticated, (req, res) => res.render("welcome"));
 
-// Dashboard
-router.get("/dashboard", ensureAuthenticated, (req, res) =>
-    res.render("dashboard", {
+//Exchange Page
+router.get("/exchange", ensureAuthenticated, (req, res) =>
+    res.render("exchange", {
+        user: req.user,
+    })
+);
+
+//Event Page
+router.get("/event", ensureAuthenticated, (req, res) =>
+    res.render("event", {
+        user: req.user,
+    })
+);
+
+//Groupname Page
+router.get("/groupname", ensureAuthenticated, (req, res) =>
+    res.render("groupname", {
         user: req.user,
     })
 );
